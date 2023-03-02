24ab36c57cfff3d29cbdaed3e5bb96d0863535ce
Kell0x <ermichardiere@gmail.com>
2023-03-02 17:33:19 +0100

    Update registerModuleSettings.js

--- src/utils/registerModuleSettings.js
+++ src/utils/registerModuleSettings.js
@@ -30,6 +30,21 @@ export default function registerModuleSettings() {
     type: Boolean,
     onChange: (value) => log.setDebug(value),
   });
+  
+  // Set grid distance that is considered "within proximity"
+    game.settings.register(moduleName, "audioDistance", {
+        name: `${moduleName}.settings.audioDistance.name`,
+        hint: `${moduleName}.settings.audioDistance.hint`,
+        scope: "world",
+        config: true,
+        type: Number,
+        default: 30,
+        range: {
+            min: 5,
+            max: 200,
+            step: 5
+        }
+    });
 
   // Set the initial debug level
   log.setDebug(game.settings.get(MODULE_NAME, "debug"));
